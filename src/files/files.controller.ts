3aa7845ff3090596fc7d7c18ee005e7d4b07d739
Artem Ivanov <ivanov@hostco.ru>
2023-11-24 15:41:12 +0500

    multer and file uploads

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body, UseInterceptors, UploadedFile, ParseFilePipe, MaxFileSizeValidator } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { CreateFileDto } from './dto/create-file.dto';
-import { UpdateFileDto } from './dto/update-file.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+import { storage } from './storage'
 
 @Controller('files')
 // тег для swagger
@@ -11,27 +13,32 @@ export class FilesController {
 	constructor(private readonly filesService: FilesService) { }
 
 	@Post()
-	create(@Body() createFileDto: CreateFileDto) {
-		return this.filesService.create(createFileDto);
+	// декоратор для работы с файлами
+	@UseInterceptors(FileInterceptor('file', { storage: storage }))
+	// настройка swagger для загрузки файлов
+	@ApiConsumes('multipart/form-data') // тип запроса
+	@ApiBody({ // сама настройка swagger
+		schema: {
+			type: 'object',
+			properties: {
+				file: {
+					type: 'string',
+					format: 'binary',
+				},
+			}
+		}
+	})
+	create(@UploadedFile(
+		new ParseFilePipe(
+			{
+				validators: [
+					// валидация на размер файла
+					new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 5 }), // 5 мб
+				]
+			}
+		)
+	) file: Express.Multer.File) {
+		return file;
 	}
 
-	@Get()
-	findAll() {
-		return this.filesService.findAll();
-	}
-
-	@Get(':id')
-	findOne(@Param('id') id: string) {
-		return this.filesService.findOne(+id);
-	}
-
-	@Patch(':id')
-	update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
-		return this.filesService.update(+id, updateFileDto);
-	}
-
-	@Delete(':id')
-	remove(@Param('id') id: string) {
-		return this.filesService.remove(+id);
-	}
 }
