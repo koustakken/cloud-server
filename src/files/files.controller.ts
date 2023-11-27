12c4f8597c0633eb52ea2123e2c98deeaa448290
Artem Ivanov <ivanov@hostco.ru>
2023-11-27 11:21:51 +0500

    end work with files, need resolve problems auth user

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Post, Body, UseInterceptors, UploadedFile, ParseFilePipe, MaxFileSizeValidator, Get, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, UseInterceptors, UploadedFile, ParseFilePipe, MaxFileSizeValidator, Get, UseGuards, Query, Delete } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 import { storage } from './storage'
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { UserId } from 'src/decorators/user-id.decorator';
+
+import { FileType } from './entities/file.entity';
 
 @Controller('files')
 // тег для swagger
@@ -15,8 +18,8 @@ export class FilesController {
 	constructor(private readonly filesService: FilesService) { }
 
 	@Get()
-	findAll() {
-		return this.filesService.findAll();
+	findAll(@UserId() userId: number, @Query('type') fileType: FileType) {
+		return this.filesService.findAll(userId, fileType);
 	}
 
 	@Post()
@@ -44,8 +47,12 @@ export class FilesController {
 				]
 			}
 		)
-	) file: Express.Multer.File) {
-		return file;
+	) file: Express.Multer.File, @UserId() userId: number) {
+		return this.filesService.create(file, userId);
 	}
 
+	@Delete()
+	remove(@UserId() userId: number, @Query('id') ids: string) {
+		return this.filesService.remove(userId, ids);
+	}
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { FileEntity } from './entities/file.entity';
 import { Repository } from 'typeorm';
 
+import { FileType } from './entities/file.entity';
+
 @Injectable()
 export class FilesService {
 	constructor(
@@ -10,7 +12,37 @@ export class FilesService {
 		private fileRepository: Repository<FileEntity>
 	) { }
 
-	findAll() {
-		return this.fileRepository.find();
+	findAll(userId: number, fileType: FileType) {
+		const qb = this.fileRepository.createQueryBuilder('file');
+		qb.where('file.userId = :userId', { userId });
+
+		if (fileType === FileType.PHOTOS) {
+			qb.andWhere('file.mimetype ILIKE :type', { mimetype: '%image%' });
+		}
+
+		if (fileType === FileType.TRASH) {
+			qb.andWhere('file.deletedAt IS NOT NULL');
+		}
+
+		return qb.getMany();
+	}
+
+	create(file: Express.Multer.File, userId: number) {
+		return this.fileRepository.save({
+			fileName: file.filename,
+			originalName: file.originalname,
+			size: file.size,
+			mimetype: file.mimetype,
+			user: { id: userId }
+		})
+	}
+
+	remove(userId: number, ids: string) {
+		const idsArray = ids.split(',');
+		const qb = this.fileRepository.createQueryBuilder('file');
+
+		qb.where('id IN (:...ids) AND userId = :userId', { ids: idsArray, userId });
+
+		return qb.softDelete().execute();
 	}
 }
